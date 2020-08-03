fb666a462fe696e1b8e137dc478c55d894d596a4
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-03 12:49:46 -0400

    Complete user story 3

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,4 +2,11 @@ class PassengersController < ApplicationController
   def show
     @passenger = Passenger.find(params[:id])
   end
+
+  def update
+    passenger = Passenger.find(params[:id])
+    flight = Flight.where({number: params[:flight_number].to_i}).first
+    FlightPassenger.create(flight_id: flight.id, passenger_id: passenger.id)
+    redirect_to "/passengers/#{passenger.id}"
+  end
 end
