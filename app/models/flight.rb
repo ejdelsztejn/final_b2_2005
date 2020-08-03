f5559f72882776ea4f669a2b71903d9d42e10548
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-03 12:59:42 -0400

    Complete user story 4

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,4 +4,12 @@ class Flight < ApplicationRecord
   belongs_to :airline
   has_many :flight_passengers
   has_many :passengers, through: :flight_passengers
+
+  def minors
+    passengers.where(age: 0...18).count
+  end
+
+  def adults
+    passengers.where(age: 18..Float::INFINITY).count
+  end
 end
